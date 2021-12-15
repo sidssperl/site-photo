0ff8c1a0042f0ee73140b5d693cbf5688540d9b7
sidssperl <95041416+sidssperl@users.noreply.github.com>
2021-12-15 23:50:27 +0300

    Add files via upload

---
+++ portfolio/lovestory/js/script.js
@@ -0,0 +1,400 @@
+$('.popup').magnificPopup({
+    items: [
+      {
+        src: 'cat/eg/01.jpg',
+        title: 'EVGEN&GALA'
+      },
+	     {
+        src: 'cat/eg/02.jpg',
+        title: 'EVGEN&GALA'
+      },
+	     {
+        src: 'cat/eg/03.jpg',
+        title: 'EVGEN&GALA'
+      },
+	     {
+        src: 'cat/eg/04.jpg',
+        title: 'EVGEN&GALA'
+      },
+      {
+        src: 'cat/eg/05.jpg',
+        title: 'EVGEN&GALA'
+      },
+	     {
+        src: 'cat/eg/06.jpg',
+        title: 'EVGEN&GALA'
+      },
+	     {
+        src: 'cat/eg/07.jpg',
+        title: 'EVGEN&GALA'
+      },
+	     {
+        src: 'cat/eg/08.jpg',
+        title: 'EVGEN&GALA'
+      },
+      {
+        src: 'cat/eg/09.jpg',
+        title: 'EVGEN&GALA'
+      },
+       {
+        src: 'cat/eg/10.jpg',
+        title: 'EVGEN&GALA'
+      },
+      {
+        src: 'cat/eg/11.jpg',
+        title: 'EVGEN&GALA'
+      },
+	     {
+        src: 'cat/eg/12.jpg',
+        title: 'EVGEN&GALA'
+      },
+	     {
+        src: 'cat/eg/13.jpg',
+        title: 'EVGEN&GALA'
+      },
+	     {
+        src: 'cat/eg/14.jpg',
+        title: 'EVGEN&GALA'
+      },
+      {
+        src: 'cat/eg/15.jpg',
+        title: 'EVGEN&GALA'
+      },
+	     {
+        src: 'cat/eg/16.jpg',
+        title: 'EVGEN&GALA'
+      },
+	     {
+        src: 'cat/eg/17.jpg',
+        title: 'EVGEN&GALA'
+      },
+	     {
+        src: 'cat/eg/18.jpg',
+        title: 'EVGEN&GALA'
+      },
+      {
+        src: 'cat/eg/19.jpg',
+        title: 'EVGEN&GALA'
+      },
+       {
+        src: 'cat/eg/20.jpg',
+        title: 'EVGEN&GALA'
+      },
+      {
+        src: 'cat/eg/21.jpg',
+        title: 'EVGEN&GALA'
+      },
+       {
+        src: 'cat/eg/22.jpg',
+        title: 'EVGEN&GALA'
+      },
+       {
+        src: 'cat/eg/23.jpg',
+        title: 'EVGEN&GALA'
+      },
+       {
+        src: 'cat/eg/24.jpg',
+        title: 'EVGEN&GALA'
+      },
+      {
+        src: 'cat/eg/25.jpg',
+        title: 'EVGEN&GALA'
+      },
+      {
+        src: 'cat/eg/26.jpg',
+        title: 'EVGEN&GALA'
+      },
+       {
+        src: 'cat/eg/27.jpg',
+        title: 'EVGEN&GALA'
+      },
+      {
+        src: 'cat/eg/28.jpg',
+        title: 'EVGEN&GALA'
+      },
+       {
+        src: 'cat/eg/29.jpg',
+        title: 'EVGEN&GALA'
+      },
+       {
+        src: 'cat/eg/30.jpg',
+        title: 'EVGEN&GALA'
+      },
+       {
+        src: 'cat/eg/31.jpg',
+        title: 'EVGEN&GALA'
+      },
+      {
+        src: 'cat/eg/32.jpg',
+        title: 'EVGEN&GALA'
+      },
+      {
+        src: 'cat/eg/33.jpg',
+        title: 'EVGEN&GALA'
+      },
+       {
+        src: 'cat/eg/34.jpg',
+        title: 'EVGEN&GALA'
+      },
+      {
+        src: 'cat/eg/35.jpg',
+        title: 'EVGEN&GALA'
+      },
+       {
+        src: 'cat/eg/36.jpg',
+        title: 'EVGEN&GALA'
+      },
+       {
+        src: 'cat/eg/37.jpg',
+        title: 'EVGEN&GALA'
+      },
+       {
+        src: 'cat/eg/38.jpg',
+        title: 'EVGEN&GALA'
+      },
+      {
+        src: 'cat/eg/39.jpg',
+        title: 'EVGEN&GALA'
+      },
+        ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
+$('.popup1').magnificPopup({
+  items: [
+    {
+      src: 'cat/rm/01.jpg',
+      title: 'ROMAN&MIROSLAVA'
+    },
+     {
+       src: 'cat/rm/02.jpg',
+       title: 'ROMAN&MIROSLAVA'
+    },
+     {
+       src: 'cat/rm/03.jpg',
+       title: 'ROMAN&MIROSLAVA'
+    },
+     {
+       src: 'cat/rm/04.jpg',
+       title: 'ROMAN&MIROSLAVA'
+    },
+    {
+      src: 'cat/rm/05.jpg',
+      title: 'ROMAN&MIROSLAVA'
+    },
+     {
+       src: 'cat/rm/06.jpg',
+       title: 'ROMAN&MIROSLAVA'
+    },
+     {
+       src: 'cat/rm/07.jpg',
+       title: 'olga'
+    },
+     {
+       src: 'cat/rm/08.jpg',
+       title: 'ROMAN&MIROSLAVA'
+    },
+    {
+      src: 'cat/rm/09.jpg',
+      title: 'ROMAN&MIROSLAVA'
+    },
+     {
+       src: 'cat/rm/10.jpg',
+       title: 'ROMAN&MIROSLAVA'
+    },
+    {
+      src: 'cat/rm/11.jpg',
+      title: 'ROMAN&MIROSLAVA'
+    },
+     {
+       src: 'cat/rm/12.jpg',
+       title: 'ROMAN&MIROSLAVA'
+    },
+     {
+       src: 'cat/rm/13.jpg',
+       title: 'ROMAN&MIROSLAVA'
+    },
+     {
+       src: 'cat/rm/14.jpg',
+       title: 'ROMAN&MIROSLAVA'
+    },
+    {
+      src: 'cat/rm/15.jpg',
+      title: 'ROMAN&MIROSLAVA'
+    },
+     {
+       src: 'cat/rm/16.jpg',
+       title: 'ROMAN&MIROSLAVA'
+    },
+     {
+       src: 'cat/rm/17.jpg',
+       title: 'ROMAN&MIROSLAVA'
+    },
+     {
+       src: 'cat/rm/18.jpg',
+       title: 'ROMAN&MIROSLAVA'
+    },
+    {
+      src: 'cat/rm/19.jpg',
+      title: 'ROMAN&MIROSLAVA'
+    },
+     {
+       src: 'cat/rm/20.jpg',
+       title: 'ROMAN&MIROSLAVA'
+    },
+    {
+      src: 'cat/rm/21.jpg',
+      title: 'ROMAN&MIROSLAVA'
+     },
+     {
+        src: 'cat/rm/22.jpg',
+        title: 'ROMAN&MIROSLAVA'
+     },
+     {
+          src: 'cat/rm/23.jpg',
+          title: 'ROMAN&MIROSLAVA'
+     },
+    {
+      src: 'cat/rm/24.jpg',
+      title: 'ROMAN&MIROSLAVA'
+    },
+    {
+      src: 'cat/rm/25.jpg',
+      title: 'ROMAN&MIROSLAVA'
+   },
+   {
+     src: 'cat/rm/26.jpg',
+     title: 'ROMAN&MIROSLAVA'
+   },
+    {
+      src: 'cat/rm/27.jpg',
+      title: 'ROMAN&MIROSLAVA'
+   },
+    {
+      src: 'cat/rm/28.jpg',
+      title: 'ROMAN&MIROSLAVA'
+   },
+    {
+      src: 'cat/rm/29.jpg',
+      title: 'ROMAN&MIROSLAVA'
+   },
+   {
+     src: 'cat/rm/30.jpg',
+     title: 'ROMAN&MIROSLAVA'
+   },
+    {
+      src: 'cat/rm/31.jpg',
+      title: 'ROMAN&MIROSLAVA'
+   },
+   {
+     src: 'cat/rm/32.jpg',
+     title: 'ROMAN&MIROSLAVA'
+   },
+   {
+     src: 'cat/rm/33.jpg',
+     title: 'ROMAN&MIROSLAVA'
+  },
+   {
+     src: 'cat/rm/34.jpg',
+     title: 'ROMAN&MIROSLAVA'
+  },
+   ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
+$('.popup2').magnificPopup({
+  items: [
+    {
+      src: 'cat/ss/01.jpg',
+      title: 'SERGE&SASHA'
+    },
+     {
+       src: 'cat/ss/02.jpg',
+       title: 'SERGE&SASHA'
+    },
+     {
+       src: 'cat/ss/03.jpg',
+       title: 'SERGE&SASHA'
+    },
+     {
+       src: 'cat/ss/04.jpg',
+       title: 'SERGE&SASHA'
+    },
+    {
+      src: 'cat/ss/05.jpg',
+      title: 'SERGE&SASHA'
+    },
+     {
+       src: 'cat/ss/06.jpg',
+       title: 'SERGE&SASHA'
+    },
+     {
+       src: 'cat/ss/07.jpg',
+       title: 'SERGE&SASHA'
+    },
+     {
+       src: 'cat/ss/08.jpg',
+       title: 'SERGE&SASHA'
+    },
+    {
+      src: 'cat/ss/09.jpg',
+      title: 'SERGE&SASHA'
+    },
+     {
+       src: 'cat/ss/10.jpg',
+       title: 'SERGE&SASHA'
+    },
+    {
+      src: 'cat/ss/11.jpg',
+      title: 'SERGE&SASHA'
+    },
+     {
+       src: 'cat/ss/12.jpg',
+       title: 'SERGE&SASHA'
+    },
+     {
+       src: 'cat/ss/13.jpg',
+       title: 'SERGE&SASHA'
+    },
+     {
+       src: 'cat/ss/14.jpg',
+       title: 'SERGE&SASHA'
+    },
+    {
+      src: 'cat/ss/15.jpg',
+      title: 'SERGE&SASHA'
+    },
+     {
+       src: 'cat/ss/16.jpg',
+       title: 'SERGE&SASHA'
+    },
+     {
+       src: 'cat/ss/17.jpg',
+       title: 'SERGE&SASHA'
+    },
+     {
+       src: 'cat/ss/18.jpg',
+       title: 'SERGE&SASHA'
+    },
+    {
+      src: 'cat/ss/19.jpg',
+      title: 'SERGE&SASHA'
+    },
+     {
+       src: 'cat/ss/20.jpg',
+       title: 'SERGE&SASHA'
+    },
+    {
+      src: 'cat/ss/21.jpg',
+      title: 'SERGE&SASHA'
+     },
+     ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
