5995208bdedecb520a5b4c422ccf648d63a12dc2
sidssperl <95041416+sidssperl@users.noreply.github.com>
2021-12-15 23:48:21 +0300

    Add files via upload

---
+++ portfolio/sport/js/script.js
@@ -0,0 +1,88 @@
+$('.popup').magnificPopup({
+    items: [
+      {
+        src: 'cat/marina/01.jpg',
+        title: 'MARINA'
+      },
+	     {
+        src: 'cat/marina/02.jpg',
+        title: 'MARINA'
+      },
+	     {
+        src: 'cat/marina/03.jpg',
+        title: 'MARINA'
+      },
+	     {
+        src: 'cat/marina/04.jpg',
+        title: 'MARINA'
+      },
+      {
+        src: 'cat/marina/05.jpg',
+        title: 'MARINA'
+      },
+	     {
+        src: 'cat/marina/06.jpg',
+        title: 'MARINA'
+      },
+	     {
+        src: 'cat/marina/07.jpg',
+        title: 'MARINA'
+      },
+	     {
+        src: 'cat/marina/08.jpg',
+        title: 'MARINA'
+      },
+      {
+        src: 'cat/marina/09.jpg',
+        title: 'MARINA'
+      },
+      ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
+$('.popup1').magnificPopup({
+  items: [
+    {
+      src: 'cat/tori/01.jpg',
+      title: 'VICTORIA'
+    },
+     {
+       src: 'cat/tori/02.jpg',
+       title: 'VICTORIA'
+    },
+     {
+       src: 'cat/tori/03.jpg',
+       title: 'VICTORIA'
+    },
+     {
+       src: 'cat/tori/04.jpg',
+       title: 'VICTORIA'
+    },
+    {
+      src: 'cat/tori/05.jpg',
+      title: 'VICTORIA'
+    },
+     {
+       src: 'cat/tori/06.jpg',
+       title: 'VICTORIA'
+    },
+     {
+       src: 'cat/tori/07.jpg',
+       title: 'VICTORIA'
+    },
+    {
+      src: 'cat/tori/08.jpg',
+      title: 'VICTORIA'
+   },
+    {
+      src: 'cat/tori/09.jpg',
+      title: 'VICTORIA'
+   },
+     ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
