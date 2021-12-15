7c4965417fb233363b570f300df990316b62536b
sidssperl <95041416+sidssperl@users.noreply.github.com>
2021-12-15 23:43:07 +0300

    Add files via upload

---
+++ portfolio/boudoir/js/script.js
@@ -0,0 +1,232 @@
+$('.popup').magnificPopup({
+    items: [
+      {
+        src: 'cat/anastasia/01.jpg',
+        title: 'anastasia'
+      },
+	     {
+        src: 'cat/anastasia/02.jpg',
+        title: 'anastasia'
+      },
+	     {
+        src: 'cat/anastasia/03.jpg',
+        title: 'anastasia'
+      },
+	     {
+        src: 'cat/anastasia/04.jpg',
+        title: 'anastasia'
+      },
+      {
+        src: 'cat/anastasia/05.jpg',
+        title: 'anastasia'
+      },
+	     {
+        src: 'cat/anastasia/06.jpg',
+        title: 'anastasia'
+      },
+	     {
+        src: 'cat/anastasia/07.jpg',
+        title: 'anastasia'
+      },
+	     {
+        src: 'cat/anastasia/08.jpg',
+        title: 'anastasia'
+      },
+      {
+        src: 'cat/anastasia/09.jpg',
+        title: 'anastasia'
+      },
+       {
+        src: 'cat/anastasia/10.jpg',
+        title: 'anastasia'
+      },
+      {
+        src: 'cat/anastasia/11.jpg',
+        title: 'anastasia'
+      },
+	     {
+        src: 'cat/anastasia/12.jpg',
+        title: 'anastasia'
+      },
+	     {
+        src: 'cat/anastasia/13.jpg',
+        title: 'anastasia'
+      },
+	     {
+        src: 'cat/anastasia/14.jpg',
+        title: 'anastasia'
+      },
+      {
+        src: 'cat/anastasia/15.jpg',
+        title: 'anastasia'
+      },
+	     {
+        src: 'cat/anastasia/16.jpg',
+        title: 'anastasia'
+      },
+	     {
+        src: 'cat/anastasia/17.jpg',
+        title: 'anastasia'
+      },
+	     {
+        src: 'cat/anastasia/18.jpg',
+        title: 'anastasia'
+      },
+      {
+        src: 'cat/anastasia/19.jpg',
+        title: 'anastasia'
+      },
+       {
+        src: 'cat/anastasia/20.jpg',
+        title: 'anastasia'
+      },
+      {
+        src: 'cat/anastasia/21.jpg',
+        title: 'anastasia'
+      },
+       {
+        src: 'cat/anastasia/22.jpg',
+        title: 'anastasia'
+      },
+       {
+        src: 'cat/anastasia/23.jpg',
+        title: 'anastasia'
+      },
+       {
+        src: 'cat/anastasia/24.jpg',
+        title: 'anastasia'
+      },
+      {
+        src: 'cat/anastasia/25.jpg',
+        title: 'anastasia'
+      },
+        ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
+$('.popup1').magnificPopup({
+  items: [
+    {
+      src: 'cat/olga/01.jpg',
+      title: 'olga'
+    },
+     {
+       src: 'cat/olga/02.jpg',
+       title: 'olga'
+    },
+     {
+       src: 'cat/olga/03.jpg',
+       title: 'olga'
+    },
+     {
+       src: 'cat/olga/04.jpg',
+       title: 'olga'
+    },
+    {
+      src: 'cat/olga/05.jpg',
+      title: 'olga'
+    },
+     {
+       src: 'cat/olga/06.jpg',
+       title: 'olga'
+    },
+     {
+       src: 'cat/olga/07.jpg',
+       title: 'olga'
+    },
+     {
+       src: 'cat/olga/08.jpg',
+       title: 'olga'
+    },
+    {
+      src: 'cat/olga/09.jpg',
+      title: 'olga'
+    },
+     {
+       src: 'cat/olga/10.jpg',
+       title: 'olga'
+    },
+    {
+      src: 'cat/olga/11.jpg',
+      title: 'olga'
+    },
+     {
+       src: 'cat/olga/12.jpg',
+       title: 'olga'
+    },
+     {
+       src: 'cat/olga/13.jpg',
+       title: 'olga'
+    },
+     {
+       src: 'cat/olga/14.jpg',
+       title: 'olga'
+    },
+    {
+      src: 'cat/olga/15.jpg',
+      title: 'olga'
+    },
+     {
+       src: 'cat/olga/16.jpg',
+       title: 'olga'
+    },
+     {
+       src: 'cat/olga/17.jpg',
+       title: 'olga'
+    },
+     {
+       src: 'cat/olga/18.jpg',
+       title: 'olga'
+    },
+    {
+      src: 'cat/olga/19.jpg',
+      title: 'olga'
+    },
+     {
+       src: 'cat/olga/20.jpg',
+       title: 'olga'
+    },
+    {
+      src: 'cat/olga/24.jpg',
+      title: 'olga'
+    },
+     ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
+$('.popup2').magnificPopup({
+    items: [
+      {
+        src: 'cat/kira/01.jpg',
+        title: 'kira'
+      },
+	    {
+        src: 'cat/kira/02.jpg',
+        title: 'kira'
+      },
+    	{
+        src: 'cat/kira/03.jpg',
+        title: 'kira'
+      },
+	    {
+        src: 'cat/kira/04.jpg',
+        title: 'kira'
+      },
+	    {
+        src: 'cat/kira/05.jpg',
+        title: 'kira'
+      },
+      {
+      src: 'cat/kira/06.jpg',
+      title: 'kira'
+          },
+        ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
