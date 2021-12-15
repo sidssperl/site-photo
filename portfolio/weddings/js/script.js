c48066cf9adb4d9e47e95a22e72624e9c325312d
sidssperl <95041416+sidssperl@users.noreply.github.com>
2021-12-15 23:45:17 +0300

    Add files via upload

---
+++ portfolio/weddings/js/script.js
@@ -0,0 +1,160 @@
+$('.popup').magnificPopup({
+    items: [
+      {
+        src: 'cat/sa/01.jpg',
+        title: 'SERGE&ANA'
+      },
+	     {
+        src: 'cat/sa/02.jpg',
+        title: 'SERGE&ANA'
+      },
+	     {
+        src: 'cat/sa/03.jpg',
+        title: 'SERGE&ANA'
+      },
+	     {
+        src: 'cat/sa/04.jpg',
+        title: 'SERGE&ANA'
+      },
+      {
+        src: 'cat/sa/05.jpg',
+        title: 'SERGE&ANA'
+      },
+	     {
+        src: 'cat/sa/06.jpg',
+        title: 'SERGE&ANA'
+      },
+	     {
+        src: 'cat/sa/07.jpg',
+        title: 'SERGE&ANA'
+      },
+	     {
+        src: 'cat/sa/08.jpg',
+        title: 'SERGE&ANA'
+      },
+      {
+        src: 'cat/sa/09.jpg',
+        title: 'SERGE&ANA'
+      },
+      {
+        src: 'cat/sa/10.jpg',
+        title: 'SERGE&ANA'
+      },
+       {
+        src: 'cat/sa/11.jpg',
+        title: 'SERGE&ANA'
+      },
+       {
+        src: 'cat/sa/12.jpg',
+        title: 'SERGE&ANA'
+      },
+       {
+        src: 'cat/sa/13.jpg',
+        title: 'SERGE&ANA'
+      },
+      {
+        src: 'cat/sa/14.jpg',
+        title: 'SERGE&ANA'
+      },
+       {
+        src: 'cat/sa/15.jpg',
+        title: 'SERGE&ANA'
+      },
+       {
+        src: 'cat/sa/16.jpg',
+        title: 'SERGE&ANA'
+      },
+       {
+        src: 'cat/sa/17.jpg',
+        title: 'SERGE&ANA'
+      },
+      {
+        src: 'cat/sa/18.jpg',
+        title: 'SERGE&ANA'
+      },
+      {
+       src: 'cat/sa/19.jpg',
+       title: 'SERGE&ANA'
+     },
+     {
+       src: 'cat/sa/20.jpg',
+       title: 'SERGE&ANA'
+     },
+      {
+       src: 'cat/sa/21.jpg',
+       title: 'SERGE&ANA'
+     },
+      {
+       src: 'cat/sa/22.jpg',
+       title: 'SERGE&ANA'
+     },
+      ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
+$('.popup1').magnificPopup({
+  items: [
+    {
+      src: 'cat/ja/01.jpg',
+      title: 'JULY&ANTON'
+    },
+     {
+       src: 'cat/ja/02.jpg',
+       title: 'JULY&ANTON'
+    },
+     {
+       src: 'cat/ja/03.jpg',
+       title: 'JULY&ANTON'
+    },
+     {
+       src: 'cat/ja/04.jpg',
+       title: 'JULY&ANTON'
+    },
+    {
+      src: 'cat/ja/05.jpg',
+      title: 'JULY&ANTON'
+    },
+     {
+       src: 'cat/ja/06.jpg',
+       title: 'JULY&ANTON'
+    },
+     {
+       src: 'cat/ja/07.jpg',
+       title: 'JULY&ANTON'
+    },
+     ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
+$('.popup2').magnificPopup({
+  items: [
+    {
+      src: 'cat/av/01.jpg',
+      title: 'ANDREW&TORI'
+    },
+     {
+       src: 'cat/av/02.jpg',
+       title: 'ANDREW&TORI'
+    },
+     {
+       src: 'cat/av/03.jpg',
+       title: 'ANDREW&TORI'
+    },
+     {
+       src: 'cat/av/04.jpg',
+       title: 'ANDREW&TORI'
+    },
+    {
+      src: 'cat/av/05.jpg',
+      title: 'ANDREW&TORI'
+    },
+     ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
