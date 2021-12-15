722cf94c67edf57087a832b1d94f8538972a0e8c
sidssperl <95041416+sidssperl@users.noreply.github.com>
2021-12-15 23:41:34 +0300

    Add files via upload

---
+++ portfolio/beauty/js/script.js
@@ -0,0 +1,204 @@
+$('.popup').magnificPopup({
+    items: [
+      {
+        src: 'cat/anastasia/01.jpg',
+        title: 'anastasia'
+      },
+	{
+        src: 'cat/anastasia/02.jpg',
+        title: 'anastasia'
+      },
+	{
+        src: 'cat/anastasia/03.jpg',
+        title: 'anastasia'
+      },
+	{
+        src: 'cat/anastasia/04.jpg',
+        title: 'anastasia'
+      },
+        ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
+$('.popup1').magnificPopup({
+    items: [
+      {
+        src: 'cat/nataly/01.jpg',
+        title: 'nataly'
+      },
+	{
+        src: 'cat/nataly/02.jpg',
+        title: 'nataly'
+      },
+	{
+        src: 'cat/nataly/03.jpg',
+        title: 'nataly'
+      },
+	{
+        src: 'cat/nataly/04.jpg',
+        title: 'nataly'
+      },
+	{
+        src: 'cat/nataly/05.jpg',
+        title: 'nataly'
+      },
+	{
+        src: 'cat/nataly/06.jpg',
+        title: 'nataly'
+      },
+	{
+        src: 'cat/nataly/07.jpg',
+        title: 'nataly'
+      },
+	{
+        src: 'cat/nataly/08.jpg',
+        title: 'nataly'
+      },
+	{
+        src: 'cat/nataly/09.jpg',
+        title: 'nataly'
+      },
+	{
+        src: 'cat/nataly/10.jpg',
+        title: 'nataly'
+      },
+        ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
+$('.popup2').magnificPopup({
+    items: [
+      {
+        src: 'cat/alina/01.jpg',
+        title: 'alina'
+      },
+	{
+        src: 'cat/alina/02.jpg',
+        title: 'alina'
+      },
+	{
+        src: 'cat/alina/03.jpg',
+        title: 'alina'
+      },
+	{
+        src: 'cat/alina/04.jpg',
+        title: 'alina'
+      },
+	{
+        src: 'cat/alina/05.jpg',
+        title: 'alina'
+      },
+        ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
+$('.popup3').magnificPopup({
+    items: [
+      {
+        src: 'cat/helen/01.jpg',
+        title: 'helen'
+      },
+	{
+        src: 'cat/helen/02.jpg',
+        title: 'helen'
+      },
+	{
+        src: 'cat/helen/03.jpg',
+        title: 'helen'
+      },
+	],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
+$('.popup4').magnificPopup({
+    items: [
+      {
+        src: 'cat/kate/01.jpg',
+        title: 'kate'
+      },
+	{
+        src: 'cat/kate/02.jpg',
+        title: 'kate'
+      },
+	{
+        src: 'cat/kate/03.jpg',
+        title: 'kate'
+      },
+	{
+        src: 'cat/kate/04.jpg',
+        title: 'kate'
+      },
+	{
+        src: 'cat/kate/05.jpg',
+        title: 'kate'
+      },
+	],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
+$('.popup5').magnificPopup({
+    items: [
+      {
+        src: 'cat/nastya/01.jpg',
+        title: 'nastya'
+      },
+	{
+        src: 'cat/nastya/02.jpg',
+        title: 'nastya'
+      },
+	{
+        src: 'cat/nastya/03.jpg',
+        title: 'nastya'
+      },
+	{
+        src: 'cat/nastya/04.jpg',
+        title: 'nastya'
+      },
+	],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
+$('.popup6').magnificPopup({
+    items: [
+      {
+        src: 'cat/nataly2/01.jpg',
+        title: 'nataly'
+      },
+	     {
+        src: 'cat/nataly2/02.jpg',
+        title: 'nataly'
+      },
+	     {
+        src: 'cat/nataly2/03.jpg',
+        title: 'nataly'
+      },
+	     {
+        src: 'cat/nataly2/04.jpg',
+        title: 'nataly'
+      },
+	     {
+        src: 'cat/nataly2/05.jpg',
+        title: 'nataly'
+      },
+	     {
+        src: 'cat/nataly2/06.jpg',
+        title: 'nataly'
+      },
+        ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
