0d0ee9e27a1d350ebe131f15dfeb6a30333f6673
sidssperl <95041416+sidssperl@users.noreply.github.com>
2021-12-15 23:55:50 +0300

    Add files via upload

---
+++ portfolio/projects/js/script.js
@@ -0,0 +1,516 @@
+$('.popup').magnificPopup({
+    items: [
+      {
+        src: 'cat/alina/01.jpg',
+        title: 'alina'
+      },
+	{
+        src: 'cat/alina/02.jpg',
+        title: 'alina'
+      },
+	{
+        src: 'cat/alina/03.jpg',
+        title: 'alina'
+      },
+	{
+        src: 'cat/alina/04.jpg',
+        title: 'alina'
+      },
+	{
+        src: 'cat/alina/05.jpg',
+        title: 'alina'
+      },
+	{
+        src: 'cat/alina/06.jpg',
+        title: 'alina'
+      },
+	{
+        src: 'cat/alina/07.jpg',
+        title: 'alina'
+      },
+	{
+        src: 'cat/alina/08.jpg',
+        title: 'alina'
+      },
+	{
+        src: 'cat/alina/09.jpg',
+        title: 'alina'
+      },
+  {
+        src: 'cat/alina/10.jpg',
+        title: 'alina'
+          },
+          {
+        src: 'cat/alina/11.jpg',
+        title: 'alina'
+              },
+          {
+        src: 'cat/alina/12.jpg',
+        title: 'alina'
+              },
+          {
+        src: 'cat/alina/13.jpg',
+        title: 'alina'
+                  },
+          {
+        src: 'cat/alina/14.jpg',
+        title: 'alina'
+          },
+          {
+        src: 'cat/alina/15.jpg',
+        title: 'alina'
+          },
+        ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
+$('.popup1').magnificPopup({
+    items: [
+      {
+        src: 'cat/kira/01.jpg',
+        title: 'kira'
+      },
+	{
+        src: 'cat/kira/02.jpg',
+        title: 'kira'
+      },
+	{
+        src: 'cat/kira/03.jpg',
+        title: 'kira'
+      },
+	{
+        src: 'cat/kira/04.jpg',
+        title: 'kira'
+      },
+	{
+        src: 'cat/kira/05.jpg',
+        title: 'kira'
+      },
+	{
+        src: 'cat/kira/06.jpg',
+        title: 'kira'
+      },
+        ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
+$('.popup2').magnificPopup({
+    items: [
+      {
+        src: 'cat/nataly/01.jpg',
+        title: 'nataly'
+      },
+	{
+        src: 'cat/nataly/02.jpg',
+        title: 'nataly'
+      },
+	{
+        src: 'cat/nataly/03.jpg',
+        title: 'nataly'
+      },
+	{
+        src: 'cat/nataly/04.jpg',
+        title: 'nataly'
+      },
+	{
+        src: 'cat/nataly/05.jpg',
+        title: 'nataly'
+      },
+	{
+        src: 'cat/nataly/06.jpg',
+        title: 'nataly'
+      },
+	{
+        src: 'cat/nataly/07.jpg',
+        title: 'nataly'
+      },
+	{
+        src: 'cat/nataly/08.jpg',
+        title: 'nataly'
+      },
+	{
+        src: 'cat/nataly/09.jpg',
+        title: 'nataly'
+      },
+  {
+        src: 'cat/nataly/10.jpg',
+        title: 'nataly'
+          },
+          {
+        src: 'cat/nataly/11.jpg',
+        title: 'nataly'
+              },
+          {
+        src: 'cat/nataly/12.jpg',
+        title: 'nataly'
+              },
+          {
+        src: 'cat/nataly/13.jpg',
+        title: 'nataly'
+                  },
+          {
+        src: 'cat/nataly/14.jpg',
+        title: 'nataly'
+          },
+        ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
+$('.popup3').magnificPopup({
+    items: [
+      {
+        src: 'cat/olga/01.jpg',
+        title: 'olga'
+      },
+	{
+        src: 'cat/olga/02.jpg',
+        title: 'olga'
+      },
+	{
+        src: 'cat/olga/03.jpg',
+        title: 'olga'
+      },
+	{
+        src: 'cat/olga/04.jpg',
+        title: 'olga'
+      },
+	{
+        src: 'cat/olga/05.jpg',
+        title: 'olga'
+      },
+	{
+        src: 'cat/olga/06.jpg',
+        title: 'olga'
+      },
+	{
+        src: 'cat/olga/07.jpg',
+        title: 'olga'
+      },
+	{
+        src: 'cat/olga/08.jpg',
+        title: 'olga'
+      },
+	   ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
+$('.popup4').magnificPopup({
+    items: [
+      {
+        src: 'cat/valeria/01.jpg',
+        title: 'valeria'
+      },
+	{
+        src: 'cat/valeria/02.jpg',
+        title: 'valeria'
+      },
+	{
+        src: 'cat/valeria/03.jpg',
+        title: 'valeria'
+      },
+	{
+        src: 'cat/valeria/04.jpg',
+        title: 'valeria'
+      },
+	{
+        src: 'cat/valeria/05.jpg',
+        title: 'valeria'
+      },
+	{
+        src: 'cat/valeria/06.jpg',
+        title: 'valeria'
+      },
+	{
+        src: 'cat/valeria/07.jpg',
+        title: 'valeria'
+      },
+	{
+        src: 'cat/valeria/08.jpg',
+        title: 'valeria'
+      },
+	{
+        src: 'cat/valeria/09.jpg',
+        title: 'valeria'
+      },
+  {
+        src: 'cat/valeria/10.jpg',
+        title: 'valeria'
+          },
+          {
+        src: 'cat/valeria/11.jpg',
+        title: 'valeria'
+              },
+          {
+        src: 'cat/valeria/12.jpg',
+        title: 'valeria'
+              },
+          {
+        src: 'cat/valeria/13.jpg',
+        title: 'valeria'
+                  },
+          {
+        src: 'cat/valeria/14.jpg',
+        title: 'valeria'
+          },
+          {
+        src: 'cat/valeria/15.jpg',
+        title: 'valeria'
+          },
+        ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
+$('.popup5').magnificPopup({
+    items: [
+      {
+        src: 'cat/nastya/01.jpg',
+        title: 'nastya'
+      },
+	{
+        src: 'cat/nastya/02.jpg',
+        title: 'nastya'
+      },
+	{
+        src: 'cat/nastya/03.jpg',
+        title: 'nastya'
+      },
+	{
+        src: 'cat/nastya/04.jpg',
+        title: 'nastya'
+      },
+	{
+        src: 'cat/nastya/05.jpg',
+        title: 'nastya'
+      },
+	{
+        src: 'cat/nastya/06.jpg',
+        title: 'nastya'
+      }
+          ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
+$('.popup6').magnificPopup({
+    items: [
+      {
+        src: 'cat/art/01.jpg',
+        title: 'art'
+      },
+	     {
+        src: 'cat/art/02.jpg',
+        title: 'art'
+      },
+	     {
+        src: 'cat/art/03.jpg',
+        title: 'art'
+      },
+	     {
+        src: 'cat/art/04.jpg',
+        title: 'art'
+      },
+	     {
+        src: 'cat/art/05.jpg',
+        title: 'art'
+      },
+	     {
+        src: 'cat/art/06.jpg',
+        title: 'art'
+      },
+      {
+        src: 'cat/art/07.jpg',
+        title: 'art'
+      },
+      {
+        src: 'cat/art/08.jpg',
+        title: 'art'
+      },
+      {
+        src: 'cat/art/09.jpg',
+        title: 'art'
+      },
+      {
+        src: 'cat/art/10.jpg',
+        title: 'art'
+      },
+      {
+        src: 'cat/art/11.jpg',
+        title: 'art'
+      },
+      {
+        src: 'cat/art/12.jpg',
+        title: 'art'
+      },
+      {
+        src: 'cat/art/13.jpg',
+        title: 'art'
+      },
+	     {
+        src: 'cat/art/14.jpg',
+        title: 'art'
+      },
+	     {
+        src: 'cat/art/15.jpg',
+        title: 'art'
+      },
+	     {
+        src: 'cat/art/16.jpg',
+        title: 'art'
+      },
+	     {
+        src: 'cat/art/17.jpg',
+        title: 'art'
+      },
+	     {
+        src: 'cat/art/18.jpg',
+        title: 'art'
+      },
+      {
+      src: 'cat/art/19.jpg',
+      title: 'art'
+      },
+    	{
+      src: 'cat/art/20.jpg',
+      title: 'art'
+      }
+          ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
+$('.popup7').magnificPopup({
+    items: [
+      {
+        src: 'cat/ksusha/01.jpg',
+        title: 'ksusha'
+      },
+	     {
+        src: 'cat/ksusha/02.jpg',
+        title: 'ksusha'
+      },
+	     {
+        src: 'cat/ksusha/03.jpg',
+        title: 'ksusha'
+      },
+	     {
+        src: 'cat/ksusha/04.jpg',
+        title: 'ksusha'
+      },
+	     {
+        src: 'cat/ksusha/05.jpg',
+        title: 'ksusha'
+      },
+	     {
+        src: 'cat/ksusha/06.jpg',
+        title: 'ksusha'
+      },
+      {
+        src: 'cat/ksusha/07.jpg',
+        title: 'ksusha'
+      },
+      {
+        src: 'cat/ksusha/08.jpg',
+        title: 'ksusha'
+      },
+      {
+        src: 'cat/ksusha/09.jpg',
+        title: 'ksusha'
+      }
+          ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
+$('.popup8').magnificPopup({
+    items: [
+      {
+        src: 'cat/spring/01.jpg',
+        title: 'spring'
+      },
+	     {
+        src: 'cat/spring/02.jpg',
+        title: 'spring'
+      },
+	     {
+        src: 'cat/spring/03.jpg',
+        title: 'spring'
+      },
+	     {
+        src: 'cat/spring/04.jpg',
+        title: 'spring'
+      },
+	     {
+        src: 'cat/spring/05.jpg',
+        title: 'spring'
+      },
+	     {
+        src: 'cat/spring/06.jpg',
+        title: 'spring'
+      },
+      {
+        src: 'cat/spring/07.jpg',
+        title: 'spring'
+      },
+      {
+        src: 'cat/spring/08.jpg',
+        title: 'spring'
+      }
+      ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
+$('.popup9').magnificPopup({
+    items: [
+      {
+        src: 'cat/victory/01.jpg',
+        title: 'victory'
+      },
+	     {
+        src: 'cat/victory/02.jpg',
+        title: 'victory'
+      },
+	     {
+        src: 'cat/victory/03.jpg',
+        title: 'victory'
+      },
+	     {
+        src: 'cat/victory/04.jpg',
+        title: 'victory'
+      },
+	     {
+        src: 'cat/victory/05.jpg',
+        title: 'victory'
+      },
+	     {
+        src: 'cat/victory/06.jpg',
+        title: 'victory'
+      },
+      {
+        src: 'cat/victory/07.jpg',
+        title: 'victory'
+      },
+      {
+        src: 'cat/victory/08.jpg',
+        title: 'victory'
+      }
+      ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
